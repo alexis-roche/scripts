7a203175578d2874050e70a5ddbaff5ecd43406c
Alexis Roche <alexis.roche@gmail.com>
2011-11-10 09:15:32 +0100

    ISMRM version of multivariate segmentation

--- segmentation/multichannel_vem.py
+++ segmentation/multichannel_vem.py
@@ -1,28 +1,67 @@
 from os.path import join
 import gc
 import numpy as np
+import scipy.ndimage as nd
 import nibabel as nb
-from nipy.algorithms.segmentation._segmentation import _ve_step
+from nipy.algorithms.segmentation._segmentation import _ve_step, _gen_ve_step
 
-PATH = '/home/alexis/E/My Dropbox/AlexisTobiShare/image_data'
-SUBJECT = '2011_10_26'
-SCAN = 1
+PATH = PATH = '/home/alexis/D/Alexis/dixon'
+
+# subject-specific params
+SUBJECTS = ('2011_10_26', '2011_10_27', '2011_11_02', '2011_11_08')
+BOTTOM_CORNER = {'2011_10_26': (0, 80, 0),
+                 '2011_10_27': (0, 74, 0),
+                 '2011_10_31': (0, 80, 0),                 
+                 '2011_11_02': (0, 87, 0),
+                 '2011_11_08': (0, 79, 0)}
+
+# subject-independent params
+CHANNELS = ('flat', 'fat', 'inv2')
+TISSUES = ('air', 'gm', 'wm', 'csf', 'fat', 'dura', 'muscle')
+BRAIN_TISSUES = ('gm', 'wm', 'csf')
+## ITK-SNAP colors: clear, red, green, blue, yellow, cyan, magenta
+BETA = 0.025
+CUTOFF = 10
+NITERS = 10
+BAD_LINKS = (('air', 'csf'), ('air', 'gm'), ('air', 'wm'),
+             ('muscle', 'gm'), ('muscle', 'wm'),
+             ('dura', 'gm'), ('dura', 'wm'),
+             ('fat', 'gm'), ('fat', 'wm'), 
+             ('csf', 'air'), 
+             ('gm', 'air'), ('gm', 'muscle'), ('gm', 'dura'),
+             ('wm', 'air'), ('wm', 'muscle'), ('wm', 'dura'))
+MEANS_MP2RAGE = {'flat': {'air': 2000, 'fat': 3500, 'dura': 2500, 'muscle': 1800, 
+                          'csf': 200, 'gm': 1500, 'wm': 2800},
+                 'fat': {'air': 5, 'fat': 400, 'dura': 150, 'muscle': 40, 
+                         'csf': 5, 'gm': 5, 'wm': 10}, 
+                 'inv1': {'air': 10, 'fat': 300, 'dura': 100, 'muscle': 50, 
+                          'csf': 150, 'gm': 200, 'wm': 70},
+                 'inv2': {'air': 20, 'fat': 800, 'dura': 350, 'muscle': 500, 
+                          'csf': 150, 'gm': 500, 'wm': 500}}
+
+SIZE_OPENING = 5
+SIZE_CLOSING = 0
+THRESHOLD = 0.5
 
 
 class TissueClassifier(object):
 
-    def __init__(self, data, mu, sigma, beta=0):
+    def __init__(self, data, mu=None, sigma=None, 
+                 ppm=None, prior=None, U = None, beta=0, 
+                 bottom_corner=(0, 0, 0), top_corner=(0, 0, 0)):
 
-        ntissues = len(mu)
         data = data.squeeze()
-        if hasattr(mu[0], '__iter__'):
-            nchannels = len(mu[0])
-            space_shape = data.shape[0:-1]
-        else:
+        if not len(data.shape) in (3, 4):
+            raise ValueError('invalid input image')
+        if len(data.shape) == 3:
             nchannels = 1
             space_shape = data.shape
+        else:
+            nchannels = data.shape[-1]
+            space_shape = data.shape[0:-1]
 
-        self.slices = [slice(1, s - 1) for s in space_shape]
+        self.slices = [slice(max(bc, 1), s - max(tc, 1))\
+                           for s, bc, tc in zip(space_shape, bottom_corner, top_corner)]
         XYZ = np.mgrid[self.slices]
         XYZ = np.reshape(XYZ, (XYZ.shape[0], np.prod(XYZ.shape[1::]))).T
         self.XYZ = np.asarray(XYZ, dtype='int', order='C')
@@ -33,30 +72,56 @@ class TissueClassifier(object):
         else:
             self.data = data_msk.reshape((np.prod(data_msk.shape[0:-1]),
                                           data_msk.shape[-1]))
-        self.ppm = np.zeros(list(space_shape) + [ntissues])
-        self.ext_field = np.zeros([self.data.shape[0], ntissues])
 
-        self.mu = np.asarray(mu).reshape((ntissues, nchannels))
-        self.sigma = np.asarray(sigma).reshape((ntissues,
-                                                nchannels, nchannels))
+        if ppm == None:
+            nclasses = len(mu)
+            self.ppm = np.zeros(list(space_shape) + [nclasses])
+            self.is_ppm = False
+            self.mu = np.asarray(mu).reshape((nclasses, nchannels))
+            self.sigma = np.asarray(sigma).reshape((nclasses,
+                                                    nchannels, nchannels))
+        elif mu == None:
+            nclasses = ppm.shape[-1]
+            self.ppm = np.asarray(ppm)
+            self.is_ppm = True
+            self.mu = np.zeros((nclasses, nchannels))
+            self.sigma = np.zeros((nclasses, nchannels, nchannels))
+        else:
+            raise ValueError('missing information')
+        self.ext_field = np.zeros([self.data.shape[0], nclasses])
 
-        self.beta = float(beta)
+        if not prior == None:
+            self.prior = np.asarray(prior)[self.slices].reshape([self.data.shape[0], nclasses])
+        else:
+            self.prior = None
+        
+        self.set_energy(U, beta)
 
         # Should check whether input data is consistent with parameter
         # sizes
 
-    def ntissues(self):
-        return len(self.mu)
+    def set_energy(self, U, beta):
+        if not U == None:
+            self.U = np.asarray(U).copy()  # make sure it's C-contiguous
+        else:
+            self.U = None
+        self.beta = float(beta)
+
+    def nclasses(self):
+        return self.ppm.shape[-1]
 
     def nchannels(self):
         return self.data.shape[-1]
 
-    def vm_step(self):
+    def vm_step(self, freeze=()):
 
         print(' VM step...')
 
-        for i in range(self.ntissues()):
-            #P = self.ppm[..., i].reshape(np.prod(self.ppm.shape[0:-1]))
+        classes = range(self.nclasses())
+        for i in freeze:
+            classes.remove(i)
+
+        for i in classes:
             P = self.ppm[..., i][self.slices].ravel()
             Z = P.sum()
             tmp = self.data.T * P.T
@@ -74,7 +139,7 @@ class TissueClassifier(object):
         print(' VE step...')
 
         # Compute posterior external field (no voxel interactions)
-        for i in range(self.ntissues()):
+        for i in range(self.nclasses()):
             print('  tissue %d' % i)
             centered_data = self.data - self.mu[i]
             inv_sigma = np.linalg.inv(self.sigma[i])
@@ -84,34 +149,84 @@ class TissueClassifier(object):
             self.ext_field[:, i] = np.exp(-.5 * maha)
             self.ext_field[:, i] *= (1. / np.sqrt(det_sigma))
 
+        if not self.prior == None:
+            self.ext_field *= self.prior
         self.ext_field.clip(1e-300, 1e300, out=self.ext_field)
+
         if self.beta == 0:
             print('  ... Normalizing...')
             tmp = self.ext_field.T
             tmp /= tmp.sum(0)
-            self.ppm[:] = self.ext_field.reshape(self.ppm.shape)
+            self.ppm[self.slices][:] = self.ext_field.reshape(self.ppm[self.slices].shape)
         else:
             print('  ... MRF regularization')
-            self.ppm = _ve_step(self.ppm, self.ext_field, self.XYZ,
-                                self.beta, False, 0)
+            if self.U == None:
+                self.ppm = _ve_step(self.ppm, self.ext_field, self.XYZ,
+                                    self.beta, False, 0)
+            else:
+                self.ppm = _gen_ve_step(self.ppm, self.ext_field, self.XYZ,
+                                        self.U, self.beta)
 
         gc.enable()
         gc.collect()
 
-    def run(self, niters=10):
+    def run(self, niters=NITERS, freeze=()):
 
+        if self.is_ppm:
+            self.vm_step(freeze=freeze)
         for i in range(niters):
             print(' Iter %d/%d...' % (i + 1, niters))
             self.ve_step()
-            self.vm_step()
+            self.vm_step(freeze=freeze)
+        self.is_ppm = True
 
+def brain_mask(pbrain, threshold=THRESHOLD, size_opening=SIZE_OPENING):
+    """
+    Compute a brain mask from a crude in-brain probability map.
+    """
+    # opening
+    size = [SIZE_OPENING for i in range(3)] 
+    opening = nd.morphology.grey_opening(pbrain, size=size)
 
-def get_file_name(ids):
-    if ids == 'mp-rage':
-        f = SUBJECT + '_normal_MP-RAGE_p2'
+    # fin main connected component
+    bin_opening = (opening > THRESHOLD)
+    label, nlabels = nd.label(bin_opening)
+    imax, count_max = 0, 0
+    for i in range(1, nlabels):
+        count = np.sum(label == i)
+        if count > count_max:
+            imax, count_max = i, count
+    main_cc = np.zeros(label.shape, dtype='int8')
+    main_cc[np.where(label == imax)] = 1
+
+    # closing
+    if  SIZE_CLOSING == 0:
+        msk = main_cc
     else:
-        f = SUBJECT + '_mp2rage_2pt-dixon'
-        f += '_scan' + str(SCAN) + '_'
+        structure = np.ones([SIZE_CLOSING for i in range(3)])
+        msk = nd.morphology.binary_closing(main_cc, structure=structure)
+
+
+    # hole filling (in each plane)
+    structure = np.zeros((3, 3, 3))
+    structure[:, :, 1] = 1
+    msk = nd.morphology.binary_fill_holes(msk, structure=structure)
+    structure = np.zeros((3, 3, 3))
+    structure[:, 1, :] = 1
+    msk = nd.morphology.binary_fill_holes(msk, structure=structure)
+    structure = np.zeros((3, 3, 3))
+    structure[1, :, :] = 1
+    msk = nd.morphology.binary_fill_holes(msk, structure=structure)
+
+    return msk
+
+
+def get_file_name(ids, subject, scan=None):
+    if ids == 'mp-rage':
+        f = subject + '_normal_MP-RAGE_p2'
+    else:
+        f = subject + '_mp2rage_2pt-dixon'
+        f += '_scan' + str(scan) + '_'
         if ids == 'fat':
             f += 'combFAT'
         elif ids == 'flat':
@@ -128,9 +243,9 @@ def get_file_name(ids):
     return join(PATH, f)
 
 
-def config_mprage():
+def config_mprage(subject):
     # MPRAGE based segmentation (single channel)
-    im = nb.load(get_file_name('mp-rage'))
+    im = nb.load(get_file_name('mp-rage', subject))
     aff = im.get_affine()
     data = im.get_data()
     mu = [5., 750., 90., 180., 380.]
@@ -138,38 +253,13 @@ def config_mprage():
     return data, aff, mu, sigma
 
 
-def config_mp2rage_dixon_zobic():
-    # MP2RAGE+DIXON based segmentation (two channels)
-    im1 = nb.load(get_file_name('flat'))
-    im2 = nb.load(get_file_name('fat'))
-    aff = im1.get_affine()
-    for im in [im1, im2]:
-        print im.get_data_dtype()
-        print im.get_affine()
-    dat1 = im1.get_data().squeeze()
-    data = np.zeros(list(dat1.shape) + [2], dtype=dat1.dtype)
-    data[..., 0] = dat1
-    data[..., 1] = im2.get_data().squeeze()
-    mu = [[2000., 0.], [2000., 500.], [150., 0.], [1200., 0.], [2800., 0.]]
-    sigma = [100 * np.eye(2) for i in range(len(mu))]
-    return data, aff, mu, sigma
-
-
-def config_mp2rage_dixon(channels='all'):
-    if channels == 'all':
-        channels = ('flat', 'fat', 'inv1', 'inv2')
-    # MP2RAGE+DIXON+inversion images (4 channels)
-    # AIR, FAT, CSF, GM, WM
-    init_mu = {'flat': [2000, 2000, 150, 1200, 2800],
-               'fat': [0, 500, 0, 0, 0],
-               'inv1': [0, 400, 160, 50, 90],
-               'inv2': [0, 1000, 180, 350, 400]}
+def config_mp2rage_dixon(channels, tissues, subject, scan):
     ims = []
     mus = []
     for c in channels:
-        im = nb.load(get_file_name(c))
+        im = nb.load(get_file_name(c, subject, scan))
         ims += [im]
-        mus += [init_mu[c]]
+        mus += [[MEANS_MP2RAGE[c][t] for t in tissues]]
         print im.get_data_dtype()
         print im.get_affine()
 
@@ -179,23 +269,127 @@ def config_mp2rage_dixon(channels='all'):
     data[..., 0] = data0
     for i in range(1, len(channels)):
         data[..., i] = ims[i].get_data().squeeze()
-    mu = [[m[i] for m in mus] for i in range(5)]
-    sigma = [100 * np.eye(len(channels)) for i in range(len(mu))]
+    mu = [[m[i] for m in mus] for i in range(len(tissues))]
+    sigma = [100 * np.eye(len(channels)) for i in range(len(tissues))]
     return data, aff, mu, sigma
 
-# AIR, FAT, CSF, GM, WM
-# mu = [[5., 5.], [750., 750.], [90., 90.], [180., 180.], [380., 380.]]
-# sigma = [100 * np.eye(2) for i in range(len(mu))]
 
-# make multichannel data
-#data, aff, mu, sigma = config_mprage()
-#data, aff, mu, sigma = config_mp2rage_dixon()
-data, aff, mu, sigma = config_mp2rage_dixon(('flat', 'fat', 'inv2'))
+def config_energy(tissues, interaction=1, cutoff=CUTOFF, bad_links=None):
+    U = interaction * np.ones((len(tissues), len(tissues)))
+    U[(range(len(tissues)), range(len(tissues)))] = 0
+    if bad_links == None:
+        bad_links = ()
+    for link in bad_links:
+        if link[0] in tissues and link[1] in tissues:
+            U[tissues.index(link[0]), tissues.index(link[1])] = cutoff
+    return U
 
-# tissue classification
+
+def compute_map(ppm):
+    return ppm.argmax(-1)
+
+
+def compute_gm_vol(ppm, mask, aff):
+    i = TISSUES.index('gm')
+    count = ppm[..., i][mask].sum()
+    jacobian = np.abs(np.linalg.det(aff))
+    return count * jacobian
+
+
+def segment_mp2rage(subject, scan):
+   
+    bottom_corner = BOTTOM_CORNER[subject]
+    prefix = subject + '_scan' + str(scan) + '_'
+
+    # tissue classification
+    data, aff, mu, sigma = config_mp2rage_dixon(CHANNELS, TISSUES, subject, scan)
+
+    # First pass: standard Potts model
+    TC = TissueClassifier(data, mu=mu, sigma=sigma, beta=BETA, 
+                          bottom_corner=bottom_corner)
+    TC.run(niters=5)
+    nb.save(nb.Nifti1Image(compute_map(TC.ppm), aff), prefix + 'map0.nii')
+
+    # First pass: penalize muscle if sourrounded by wm or gm. This is
+    # basically a hack to avoid massive mis-classification of gm as
+    # muscle - a situation which cannot be handled by the mean field
+    # algorithm. Future work: use graph cuts.
+    U = config_energy(TISSUES, bad_links=(
+            ('muscle', 'wm'), ('muscle', 'gm')))
+    TC = TissueClassifier(data, mu=mu, sigma=sigma, U=U, beta=BETA,
+                          bottom_corner=bottom_corner)
+    TC.run(niters=4)
+
+    """
+    nb.save(nb.Nifti1Image(compute_map(TC.ppm), aff), prefix + 'map1.nii')
+    # Third pass: penalize all impossible connections and smooth
+    U = config_energy(TISSUES, bad_links=BAD_LINKS)
+    TC.set_energy(U, BETA)
+    TC.run(niters=5)
+    """
+    # Compute MAP
+    label = compute_map(TC.ppm)
+    nb.save(nb.Nifti1Image(label, aff), prefix + 'map.nii')
+
+    # Morphomat
+    print('Computing brain mask...')
+    pbrain = TC.ppm[..., [TISSUES.index(t) for t in BRAIN_TISSUES]].sum(-1)
+    mask = brain_mask(pbrain)
+    label[np.where(mask == 0)] = 0
+    nb.save(nb.Nifti1Image(pbrain, aff), prefix + 'pbrain.nii')
+    nb.save(nb.Nifti1Image(label, aff), prefix + 'masked_map.nii')
+
+    # Save ppm 
+    print('Saving PPM...')
+    nb.save(nb.Nifti1Image(TC.ppm, aff), prefix + 'ppm.nii')
+    
+    # Gray matter volume (mm3)
+    gm_vol = compute_gm_vol(TC.ppm, mask, aff)
+    print('Gray matter volume: %f' % gm_vol)
+    return gm_vol
+
+
+segment_mp2rage('2011_10_26', 1)
+
+#SUBJECTS = ( '2011_11_08',)  # hack
+
+
 """
-TC = TissueClassifier(data, mu, sigma, beta=0.2)
-TC.run()
-data = TC.ppm.argmax(-1)
-nb.save(nb.Nifti1Image(data, aff), 'classif.nii')
+gm_vols = {}
+for subject in SUBJECTS:
+    for scan in (1, 2):
+        key = subject + '_scan' + str(scan) 
+        gm_vols[key] = segment_mp2rage(subject, scan)
+        print gm_vols
+"""
+"""
+def morpho(count, count_tiv, affine):
+    jacobian = np.abs(np.linalg.det(affine))
+    vol = count * jacobian
+    vol_tiv = count_tiv * jacobian
+    rvol = count / float(count_tiv)
+    x = np.array((count, count_tiv, vol, vol_tiv, rvol, jacobian))
+    print x
+    return x
+
+y = []
+for subject in SUBJECTS:
+    for scan in (1, 2):
+        print('Subject: %s, Scan: %d' % (subject, scan))
+        im = nb.load(subject + '_scan' + str(scan) + '_ppm.nii')
+        ppm = im.get_data()
+        pbrain = ppm[..., [TISSUES.index(t) for t in BRAIN_TISSUES]].sum(-1)
+        mask = brain_mask(pbrain)
+        i = TISSUES.index('gm')
+        count = ppm[..., i][mask].sum()
+        count_tiv = mask.sum()
+        y += [morpho(count, count_tiv, im.get_affine())]
+    print('Subject: %s, MR-RAGE' % subject)
+    im = nb.load(join(PATH, 'test', 'gm_WIP542E_v4_' + subject + '_normal_MP-RAGE_p2.nii'))
+    count = im.get_data().sum() / 1000.
+    im = nb.load(join(PATH, 'test', 'tiv_WIP542E_v4_' + subject + '_normal_MP-RAGE_p2.nii'))
+    count_tiv = (im.get_data() > 0).sum()
+    y += [morpho(count, count_tiv, im.get_affine())]
+x = np.array(y)
+np.save('toto', x)
 """
